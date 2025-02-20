8ab56f48a58a2cb9af51a800955fcca679d9fefd
kalp_1210 <rambhiakalp@gmail.com>
2025-02-20 11:35:44 +0530

    Add meals provider using Riverpod and update dependencies in pubspec

---
+++ adv_basics/lib/provider/sectionProvider9/meals_provider.dart
@@ -0,0 +1,6 @@
+import 'package:adv_basics/data/sectionData8/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mealsProvider = Provider((ref) {
+  return dummyMeals;
+});
