8304bc81e40818999fc95b193e53721ad5aceada
kalp_1210 <rambhiakalp@gmail.com>
2025-02-26 21:53:33 +0530

    Add HTTP functionality to save new grocery items; update dependencies and refactor item saving logic

--- adv_basics/lib/widgets/sectionWidget11/new_item.dart
+++ adv_basics/lib/widgets/sectionWidget11/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:adv_basics/data/sectionData11/categories.dart';
 import 'package:adv_basics/models/sectionModels11/category.dart';
 import 'package:adv_basics/models/sectionModels11/grocery_item.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,19 +25,35 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
+
+      final url = Uri.https('flutter-prep2912-default-rtdb.firebaseio.com/',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title
+          },
+        ),
       );
+
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //       id: DateTime.now().toString(),
+      //       name: _enteredName,
+      //       quantity: _enteredQuantity,
+      //       category: _selectedCategory),
+      // );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
         title: Text('Add a new item'),
